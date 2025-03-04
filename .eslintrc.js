026f8fae4851495b56c5c5bc4e15322d6771d9cc
t4ni_XP <gutchi-t@outlook.com>
2025-03-04 20:32:14 +0900

    setting lint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// .eslintrc.js
+module.exports = {
+    extends: [
+      "next/core-web-vitals",
+      "plugin:prettier/recommended"
+    ],
+    rules: {
+      // お好みで追加のルールを書いてね
+    }
+  };
+  
