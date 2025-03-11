6ad281f82e360f8d0eb93abf355f83f66d294778
t4ni_XP <gutchi-t@outlook.com>
2025-03-11 20:49:31 +0900

    make main page

---
+++ src/app/component/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Box, Card } from "@mui/material";
+
+export default function SideBar() {
+  return (
+    <Box>
+      <Card></Card>
+    </Box>
+  );
+}
